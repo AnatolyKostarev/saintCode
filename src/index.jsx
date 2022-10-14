3e374cadf377729d966912695aaa05846e3f270e
Danil <mydogfromhell666@gmail.com>
2022-10-14 18:59:48 +0300

    Add eslint (air-bnb)

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App/App';
+import './index.css';
+import i18n from './config/i18n/i18n';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+);
