e6031c34247b56577f44afbeb20c86ab0324c2ab
Danil <mydogfromhell666@gmail.com>
2022-10-14 18:15:23 +0300

    Add i18n, i18n backend

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App/App'
 import './index.css'
+import i18n from "./config/i18n/i18n";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
