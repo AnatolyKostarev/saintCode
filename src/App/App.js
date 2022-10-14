489b6c0aea73ba7a428f5643db0532eda8f25214
Danil <102468675+raveoverdose@users.noreply.github.com>
2022-10-14 18:26:26 +0300

    Add i18n, i18n backend (#3)

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,16 @@
 import cls from './App.module.sass'
+import {useTranslation} from "react-i18next";
+import {Suspense} from "react";
 
 function App() {
-  return (
-    <div className={cls.App}>
-      <h1>SaintCode</h1>
-    </div>
-  )
+    const {t, i18n} = useTranslation();
+    return (
+        <div className={cls.App}>
+            <Suspense fallback=''>
+                <h1>{t('SaintCode')}</h1>
+            </Suspense>
+        </div>
+    )
 }
 
 export default App
