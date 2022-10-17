197537bb6cd490097ec301ef7ce93a8beb5fd8e6
Anatoly <anatolykostarev@gmail.com>
2022-10-17 19:00:42 +0300

    Add Header into Pages

--- src/page/Preprogram/Preprogram.jsx
+++ src/page/Preprogram/Preprogram.jsx
@@ -1,10 +1,12 @@
 import { Container } from '../../ui/Container/Container'
 import { Section } from '../../ui/Section/Section'
+import { Header } from '../../components/Header/Header'
 import s from './Preprogram.module.sass'
 
 export const Preprogram = () => {
   return (
     <>
+      <Header />
       <Section className={s.program}>
         <Container>
           <h2>Подготовительная программа</h2>
