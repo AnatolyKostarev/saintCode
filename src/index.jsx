74463b806a547732f693049803bf4bebe1cde4f5
Anatoly <anatolykostarev@gmail.com>
2022-10-17 17:16:34 +0300

    Refactor import styles

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App/App'
-import './index.css'
+import './index.sass'
 import i18n from './config/i18n/i18n'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
