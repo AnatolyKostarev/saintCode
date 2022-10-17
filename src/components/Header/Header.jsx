32c32ab99ee5adba48050805ef39b8d4ae5e05af
Anatoly <anatolykostarev@gmail.com>
2022-10-17 17:14:40 +0300

    Add components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import clsx from 'clsx'
+import { useTranslation } from 'react-i18next'
+import s from './Header.module.sass'
+import Logo from '../../ui/Logo/Logo'
+import Phone from '../../ui/Phone/Phone'
+import Nav from '../../ui/Nav/Nav'
+import Button from '../../ui/Button/Button'
+
+export const Header = () => {
+  const { t, i18n } = useTranslation()
+
+  // For test translation
+  const toggle = async () => {
+    await i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+  }
+
+  return (
+    <header className={clsx(s.Header)}>
+      <div className={clsx(s.wrapper)}>
+        <div className={clsx(s.block)}>
+          <Logo />
+          <Phone />
+        </div>
+        <div className={clsx(s.block)}>
+          <Nav />
+          <Button
+            toggleLang={toggle}
+            text={t('Люблю поговорить, позвони мне')}
+            className={s.call}
+          />
+        </div>
+      </div>
+    </header>
+  )
+}
