c62b49fc0a300033378e9cdb86dec746dd8121ca
Anatoly <anatolykostarev@gmail.com>
2022-10-17 17:15:26 +0300

    Add components into Home Page

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,15 +1,20 @@
 import { Section } from '../../ui/Section/Section'
 import { Container } from '../../ui/Container/Container'
+import { Header } from '../../components/Header/Header'
+import { Hero } from '../../components/Hero/Hero'
 import s from './Home.module.sass'
 
 export const Home = () => {
   return (
     <>
-      <Section className={s.home}>
-        <Container>
-          <h2>Home</h2>
-        </Container>
-      </Section>
+      <Header />
+      <main>
+        <Section className={s.hero}>
+          <Container>
+            <Hero />
+          </Container>
+        </Section>
+      </main>
     </>
   )
 }
