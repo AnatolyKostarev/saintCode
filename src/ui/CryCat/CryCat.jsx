0446a337b2435bbc6269b8b31bb9163f350ff4d7
Danil <mydogfromhell666@gmail.com>
2022-10-18 23:06:49 +0300

    add notFound page

---
+++ src/ui/CryCat/CryCat.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import clsx from 'clsx'
+import s from './CryCat.module.sass'
+import Pic from './cat.webp'
+import PicX2 from './cat-2x.webp'
+
+const CryCat = () => {
+  return (
+    <div className={clsx(s.CryCat)}>
+      <img
+        className={clsx(s.CryCat__img)}
+        src={Pic}
+        srcSet={`${Pic} 1x, ${PicX2} 2x`}
+        width="450"
+        height="554"
+        alt="404 | Crying Cat - picture"
+      />
+    </div>
+  )
+}
+
+export default CryCat
---
+++ src/ui/NotFoundArticle/NotFoundArticle.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import clsx from 'clsx'
+import Title from '../Title/Title'
+import s from './NotFoundArticle.module.sass'
+import {Link} from "react-router-dom";
+import Button from "../Button/Button";
+
+const NotFoundArticle = ({className}) => {
+  return (
+    <article className={clsx(s.NotFoundArticle, className)}>
+      <Title>
+        <span className="transparent">Упс!</span>
+        {' '}
+        Очень жаль
+      </Title>
+      <span className={clsx(s.Error, 'transparent')}
+      >Мы не можем открыть эту страницу</span
+      >
+      <Link to='/'>
+        <Button className={s.btn} text='Пойдем домой'/>
+      </Link>
+    </article>
+  )
+}
+
+export default NotFoundArticle
