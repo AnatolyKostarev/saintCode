37dc35cdb4de8832c2a7d35ac2201d051b7b5dfe
Anatoly <anatolykostarev@gmail.com>
2022-10-19 22:55:44 +0300

    Replace h1

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import clsx from 'clsx'
 import { useTranslation } from 'react-i18next'
+import { Section } from '../../ui/Section/Section'
+import { Container } from '../../ui/Container/Container'
 import Title from '../../ui/Title/Title'
 import HeroArticle from '../../ui/HeroArticle/HeroArticle'
 import s from './Hero.module.sass'
@@ -8,35 +9,33 @@ import s from './Hero.module.sass'
 export const Hero = () => {
   const { t } = useTranslation()
   return (
-    <section className={clsx(s.Hero)}>
-      {/* <div className={clsx(s.container)}> */}
-      <Title>
-        {t('Перезапусти карьеру')}
-        <span className="transparent">
-          {' '}
-          ——
-          <br />
-          {t('научись программировать')}
-        </span>
-      </Title>
-      {/* </div> */}
-      <HeroArticle>
+    <Section className={s.Hero}>
+      <Container className={s.container}>
         <p>
+          {t('Перезапусти карьеру')}
+          <span className="transparent">
+            {' '}
+            ——
+            <br />
+            {t('научись программировать')}
+          </span>
+        </p>
+      </Container>
+      <HeroArticle>
+        <Title>
           {t('Школа программирования')}
           <br />
-          {' '}
           <span className="transparent">
             {t('для веб-разработчиков в формате буткемпа:')}
           </span>
           <br />
           {t('с 0 до Junior за 3 месяца,')}
           <br />
-          {' '}
           <span className="transparent">
             {t('вживую в СПб или онлайн из любой точки')}
           </span>
-        </p>
+        </Title>
       </HeroArticle>
-    </section>
+    </Section>
   )
 }
