64f9760b33a7e6998b30267c06f4f111cc23e1d0
Anatoly <anatolykostarev@gmail.com>
2022-10-20 21:14:08 +0300

    Add components into Home Page

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,18 +1,18 @@
-import { Section } from '../../ui/Section/Section'
 import { Header } from '../../components/Header/Header'
 import { Hero } from '../../components/Hero/Hero'
-import s from './Home.module.sass'
 import ForWho from '../../components/ForWho/ForWho'
+import { SchoolComparison } from '../../components/SchoolComparison/SchoolComparison'
+import { DailySchedule } from '../../components/DailySchedule/DailySchedule'
 
 export const Home = () => {
   return (
     <>
       <Header />
       <main>
-        <Section className={s.hero}>
-          <Hero />
-          <ForWho />
-        </Section>
+        <Hero />
+        <ForWho />
+        <SchoolComparison />
+        <DailySchedule />
       </main>
     </>
   )
