5bc4349a9c99aec0ce023cda651961fa235f5190
Anatoly <anatolykostarev@gmail.com>
2022-10-24 14:25:47 +0300

    Create Price Section to add into Home

---
+++ src/components/Price/Price.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Section } from '../../ui/Section/Section'
+import { Container } from '../../ui/Container/Container'
+import { SectionTitle } from '../../ui/SectionTitle/SectionTitle'
+import { PriceCard } from '../../ui/PriceCard/PriceCard'
+import { dataPrice } from './dataPrice'
+import s from './Price.module.sass'
+
+export const Price = () => {
+  return (
+    <Section className={s.Price}>
+      <Container>
+        <SectionTitle className={s.Price__title}>Сколько стоит?</SectionTitle>
+        <div className={s.Price__wrapper}>
+          {dataPrice.map(({ ...elem }) => (
+            <PriceCard {...elem} />
+          ))}
+        </div>
+      </Container>
+    </Section>
+  )
+}
