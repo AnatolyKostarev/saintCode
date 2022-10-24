7be66327fabfa7afc6740bfd3fb215f337e7c238
Anatoly <anatolykostarev@gmail.com>
2022-10-24 14:15:32 +0300

    Create PriceCard and set styles

---
+++ src/ui/PriceCard/PriceCard.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Button from '../Button/Button'
+import s from './PriceCard.module.sass'
+
+export const PriceCard = ({
+  icon,
+  start,
+  type,
+  price,
+  priceWithDiscount,
+  discountLastDay,
+  description,
+}) => {
+  return (
+    
+    <div className={s.Price__card}>
+      <div className={s.Price__header}>
+        <img src={icon} alt="offline" width="60" height="60" />
+        <p>
+          Дата старта:
+          {' '}
+          {start}
+        </p>
+      </div>
+      <p className={s.Price__type}>
+        {type}
+      </p>
+      <p className={s.Price__discount}>
+        {price}
+        {' '}
+        руб.
+      </p>
+      <p className={s.Price__real}>
+        {priceWithDiscount}
+        {' '}
+        руб.
+      </p>
+      <p className={s.Price__finalDate}>
+        Скидка до
+        {discountLastDay}
+      </p>
+      <p className={s.Price__description}>
+        {description}
+      </p>
+      <Button className={s.Price__btn} text="Записаться на курс" />
+    </div>
+  )
+}
