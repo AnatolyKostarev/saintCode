fb6f4bb34086c3307f78d014fd7e722d39abf4fa
Anatoly <anatolykostarev@gmail.com>
2022-10-26 20:42:48 +0300

    Refactor  Price Card

--- src/ui/PriceCard/PriceCard.jsx
+++ src/ui/PriceCard/PriceCard.jsx
@@ -18,31 +18,36 @@ export const PriceCard = ({
           {location}
         </p>
       </div>
-      <p className={s.Price__discount}>
-        {new Intl.NumberFormat('ru-Ru').format(price)}
-        {' '}
-        руб.
-      </p>
-      <p className={s.Price__real}>
-        {new Intl.NumberFormat('ru-Ru').format(priceWithDiscount)}
-        {' '}
-        руб.
-      </p>
-      <p className={s.Price__startDate}>
-        Дата старта:&nbsp;
-        {start}
-      </p>
-      <p className={s.Price__finalDate}>
-        Скидка до&nbsp;
-        {discountLastDay}
-      </p>
-      <p className={s.Price__type}>
-        {type}
-      </p>
-      <p className={s.Price__description}>
-        {description}
-      </p>
-      <Button className={s.Price__btn} text="Записаться на курс" />
+      <div>
+        <p className={s.Price__discount}>
+          {new Intl.NumberFormat('ru-Ru').format(price)}
+          {' '}
+          руб.
+        </p>
+        <p className={s.Price__real}>
+          {new Intl.NumberFormat('ru-Ru').format(priceWithDiscount)}
+          {' '}
+          руб.
+        </p>
+        <p className={s.Price__startDate}>
+          Дата старта:&nbsp;
+          {start}
+        </p>
+        <p className={s.Price__finalDate}>
+          Скидка до&nbsp;
+          {discountLastDay}
+        </p>
+        <p className={s.Price__type}>
+          {type}
+        </p>
+        <p className={s.Price__description}>
+          {description}
+        </p>
+      </div>
+      <div className={s.Price__wrap}>
+        <Button className={s.Price__btn} text="Записаться на курс" />
+      </div>
+   
     </div>
   )
 }
