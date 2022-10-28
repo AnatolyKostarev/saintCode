5738eb1f6becab6e32a44cb251a04bacc49c1784
Anatoly <anatolykostarev@gmail.com>
2022-10-28 10:50:45 +0300

    Refactor code, fix styles

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
-import clsx from 'clsx'
-import s from './Results.module.sass'
 import { Section } from '../../ui/Section/Section'
 import { Container } from '../../ui/Container/Container'
 import { SectionTitle } from '../../ui/SectionTitle/SectionTitle'
 import ResultsArticle from '../../ui/ResultsArticle/ResultsArticle'
-import group from './group.svg'
-import minded from './minded.svg'
-import neuron from './neuron.svg'
-import programm from './programm.svg'
-import teacher from './teacher.svg'
+import { resultData } from './resultData'
+import s from './Results.module.sass'
 
-const Results = ({ className }) => {
+const Results = () => {
   return (
-    <Section className={clsx(s.Results, className)}>
+    <Section className={s.Results}>
       <Container className={s.Results__container}>
         <div className={s.left}>
           <SectionTitle>
@@ -25,36 +20,9 @@ const Results = ({ className }) => {
           </SectionTitle>
         </div>
         <div className={s.right}>
-          <ResultsArticle
-            icon={group}
-            subtitle="Маленькую группу (6-12 человек)"
-            text="Где преподаватель может уделить внимание каждому. Если студент на чём-то застревает — с ним
-            занимаются отдельно. Если знает больше остальных — ему дают продвинутые задания"
-          />
-          <ResultsArticle
-            icon={teacher}
-            subtitle="Преподавателя"
-            text="Который реально заботится о том, что ты освоишь программу.
-            Наши наставники часто созваниваются со студентами по вечерам или выходным,
-            хотя это не входит в их обязанности"
-          />
-          <ResultsArticle
-            icon={neuron}
-            subtitle="Связи"
-            text="Сокурсники, наставники и приглашённые эксперты станут частью твоих деловых знакомств,
-            которые помогут найти работу"
-          />
-          <ResultsArticle
-            icon={minded}
-            subtitle="Единомышленников"
-            text="Потому что легче достигать целей в группе"
-          />
-          <ResultsArticle
-            className={s.end}
-            icon={programm}
-            subtitle="Программу"
-            text="На основе десятилетнего опыта ведущих буткемпов мира"
-          />
+          {resultData.map(({ ...elem }) => (
+            <ResultsArticle className={s.Results__article} {...elem} key={elem.id} />
+          ))}
         </div>
       </Container>
     </Section>
