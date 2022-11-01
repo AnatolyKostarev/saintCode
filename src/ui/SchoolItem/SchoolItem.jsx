8fa88fb325706ebcc4fadfdb2769c1c9dc620e4b
Anatoly <anatolykostarev@gmail.com>
2022-11-01 22:39:10 +0300

    Refactor component, add children prop, refactor and fix styles

--- src/ui/SchoolItem/SchoolItem.jsx
+++ src/ui/SchoolItem/SchoolItem.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import s from './SchoolItem.module.sass'
 
 export const SchoolItem = ({
-  id, title, content, onClick, show,
+  children, id, title, onClick, show,
 }) => {
   return (
-    <div className={show ? s.school__item : s.school__item__active}>
-      <button className={s.school__btn} id={id} type="button" onClick={onClick}>
-        <p>
-          {title}
-        </p>
-        <p className={show ? s.school__cross : s.school__cross__rotate}>
-          &#10006;
-        </p>
-      </button>
-      <p className={show ? s.school__content : s.school__content__active}>
-        {content}
-      </p>
-    </div>
+    <>
+      <div className={show ? s.school__item : s.school__item__active}>
+        <button
+          className={s.school__btn}
+          id={id}
+          type="button"
+          onClick={onClick}
+        >
+          <p>
+            {title}
+          </p>
+          <p className={show ? s.school__cross : s.school__cross__rotate}>
+            &#10006;
+          </p>
+        </button>
+        {children}
+      </div>
+    </>
   )
 }
