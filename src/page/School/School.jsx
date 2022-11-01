e47b255033d6349218da10b62c79679b00b88555
Anatoly <anatolykostarev@gmail.com>
2022-11-01 22:41:52 +0300

    Refactor School page, fix styles

--- src/page/School/School.jsx
+++ src/page/School/School.jsx
@@ -1,46 +1,12 @@
-import { useState } from 'react'
 import { Layout } from '../../layout'
-import { Section } from '../../ui/Section'
-import { Container } from '../../ui/Container'
-import { SectionTitle } from '../../ui/SectionTitle'
-import { SchoolItem } from '../../ui/SchoolItem'
-import s from './School.module.sass'
+import { SchoolInfo } from '../../components/SchoolInfo'
+import { Team } from '../../components/Team'
 
 export const School = () => {
-  const [show, setShow] = useState(true)
-
-  const schoolItemSwitcher = () => setShow(!show)
-
   return (
     <Layout>
-      <Section className={s.school}>
-        <Container>
-          <SectionTitle className={s.school__title}>О Школе</SectionTitle>
-          <div className={s.school__items}>
-            <SchoolItem
-              id={1}
-              title="text"
-              content="content"
-              onClick={schoolItemSwitcher}
-              show={show}
-            />
-            <SchoolItem
-              id={1}
-              title="text"
-              content="content"
-              onClick={schoolItemSwitcher}
-              show={show}
-            />
-            <SchoolItem
-              id={1}
-              title="text"
-              content="content"
-              onClick={schoolItemSwitcher}
-              show={show}
-            />
-          </div>
-        </Container>
-      </Section>
+      <SchoolInfo />
+      <Team />
     </Layout>
   )
 }
