fbbde66574345b2fca4922794eae4ff80dfe05cc
Anatoly <anatolykostarev@gmail.com>
2022-11-02 15:11:45 +0300

    Create Team component, add styles

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 import { Container } from '../../ui/Container'
 import { Section } from '../../ui/Section'
 import { SectionTitle } from '../../ui/SectionTitle'
+import { MemberCard } from '../../ui/MemberCard'
+import { MemberCardHeader } from '../../ui/MemberCard/MemberCardHeader'
+import { teamData } from './teamData'
 import s from './Team.module.sass'
 
 export const Team = () => {
   return (
-    
     <Section className={s.team}>
       <Container>
         <SectionTitle className={s.team__title}>
           Команда
         </SectionTitle>
+        <div className={s.team__wrapper}>
+          {teamData.map(member => (
+            <MemberCard key={member.id}>
+              <MemberCardHeader {...member} />
+            </MemberCard>
+          ))}
+        </div>
       </Container>
     </Section>
   )
