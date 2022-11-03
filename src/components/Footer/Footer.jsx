2a82320f576eefcfe02b24dc4339617165de7163
Anatoly <anatolykostarev@gmail.com>
2022-11-03 15:50:39 +0300

    Add map

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,15 @@ import clsx from 'clsx'
 import { Container } from '../../ui/Container/Container'
 import { FooterContacts } from '../../ui/FooterContacts'
 import { FooterCopyright } from '../../ui/FooterCopyright'
+import map from './map.png'
 import s from './Footer.module.sass'
 
 export const Footer = ({ className }) => {
   return (
     <footer className={clsx(s.Footer, className)}>
+      <div className={s.Footer__map}>
+        <img src={map} alt="map" />
+      </div>
       <Container>
         <FooterContacts />
       </Container>
