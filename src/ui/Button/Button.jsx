abee434f4fa050fc43a6a168965115f226e9bf82
Anatoly <anatolykostarev@gmail.com>
2022-11-14 15:54:08 +0500

    Add RHF validation for ConsultForm (#56)
    * Add disabled props for the Button component
    
    * Add hooks
    
    * Add init values for ConsultForm
    
    * Add space line
    
    * Add form validation by RHF
    
    * Fix and add styles
    
    * Fix rules
    
    * Uninstall Formik

--- src/ui/Button/Button.jsx
+++ src/ui/Button/Button.jsx
@@ -3,14 +3,20 @@ import clsx from 'clsx'
 import s from './Button.module.sass'
 
 export const Button = ({
-  text, className, onClick, style = {},
+  text,
+  className,
+  onClick,
+  type,
+  style = {},
+  disabled = false,
 }) => {
   return (
     <button
       className={clsx(s.Button, className)}
-      type="button"
+      type={type === 'button' ? 'button' : 'submit'}
       onClick={onClick}
       style={style}
+      disabled={disabled}
     >
       {text}
     </button>
