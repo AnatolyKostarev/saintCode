e1ccd96231465ea2834bf292269f4bbb93dcb181
Danil <mydogfromhell666@gmail.com>
2022-11-18 17:02:47 +0300

    fix sdtyles, add intetrsec component

--- src/components/IntersecPicture/IntersecPicture.jsx
+++ src/components/IntersecPicture/IntersecPicture.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import clsx from 'clsx'
 import s from './IntersecPicture.module.sass'
-import { Container } from '../../ui/Container'
 
 export const IntersecPicture = ({ className, children }) => {
   return (
