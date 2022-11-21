04fdbeb643d99881c7351e6f997e4167e86ed18d
Anatoly <anatolykostarev@gmail.com>
2022-11-21 18:31:20 +0500

    Reformat component

--- src/components/TrainingProgram/TrainingProgram.jsx
+++ src/components/TrainingProgram/TrainingProgram.jsx
@@ -20,10 +20,8 @@ export const TrainingProgram = () => {
     <Section className={s.TrainingProgram}>
       <Container>
         <div className={s.TrainingProgram__header}>
-          <SectionTitle>
-            <span>
-              {t('TrainingProgram.title_span')}
-            </span>
+          <SectionTitle className={s.TrainingProgram__header_title}>
+            <span>{t('TrainingProgram.title_span')}</span>
             <br />
             {t('TrainingProgram.title')}
           </SectionTitle>
@@ -32,89 +30,71 @@ export const TrainingProgram = () => {
               src={ThreeIcon}
               alt="three"
               width="281"
-              height="187"
+              height="206"
             />
             <p>
-              <br />
-              {t('TrainingProgram.subtitle_one')}
-              {' '}
-              <br />
-              {' '}
+              {t('TrainingProgram.subtitle_one')}{' '}
               {t('TrainingProgram.subtitle_two')}
             </p>
           </div>
         </div>
-        <div className={s.TrainingProgram__title}>
-          <p>
-            {t('TrainingProgram.task.one.title')}
-          </p>
-          <p>
-            01
-          </p>
-        </div>
-        <div className={s.TrainingProgram__tasks}>
-          <div className={s.TrainingProgram__wrapper}>
-            <List items={trainingWeb} />
+        <div className={s.border}>
+          <div className={s.TrainingProgram__title}>
+            <p>{t('TrainingProgram.task.one.title')}</p>
+            <p>01</p>
           </div>
-          <Panel
-            className={s.TrainingProgram__panel}
-            text={t('TrainingProgram.total')}
-            description={t('TrainingProgram.task.one.description')}
-          />
-        </div>
-        <div className={s.TrainingProgram__title}>
-          <p>
-            {t('TrainingProgram.task.two.title')}
-          </p>
-          <p>
-            02
-          </p>
-        </div>
-        <div className={s.TrainingProgram__tasks}>
-          <div className={s.TrainingProgram__wrapper}>
-            <List items={trainingJS} />
+          <div className={s.TrainingProgram__tasks}>
+            <div className={s.TrainingProgram__wrapper}>
+              <List items={trainingWeb} />
+            </div>
+            <Panel
+              className={s.TrainingProgram__panel}
+              text={t('TrainingProgram.total')}
+              description={t('TrainingProgram.task.one.description')}
+            />
           </div>
-          <Panel
-            className={s.TrainingProgram__panel}
-            text={t('TrainingProgram.total')}
-            description={t('TrainingProgram.task.two.description')}
-          />
-        </div>
-        <div className={s.TrainingProgram__title}>
-          <p>
-            {t('TrainingProgram.task.three.title')}
-          </p>
-          <p>
-            03
-          </p>
-        </div>
-        <div className={s.TrainingProgram__tasks}>
-          <div className={s.TrainingProgram__wrapper}>
-            <List items={trainingReact} />
+          <div className={s.TrainingProgram__title}>
+            <p>{t('TrainingProgram.task.two.title')}</p>
+            <p>02</p>
           </div>
-          <Panel
-            className={s.TrainingProgram__panel}
-            text={t('TrainingProgram.total')}
-            description={t('TrainingProgram.task.three.description')}
-          />
-        </div>
-        <div className={s.TrainingProgram__title}>
-          <p>
-            {t('TrainingProgram.task.four.title')}
-          </p>
-          <p>
-            04
-          </p>
-        </div>
-        <div className={s.TrainingProgram__tasks}>
-          <div className={s.TrainingProgram__wrapper}>
-            <List items={trainingJob} />
+          <div className={s.TrainingProgram__tasks}>
+            <div className={s.TrainingProgram__wrapper}>
+              <List items={trainingJS} />
+            </div>
+            <Panel
+              className={s.TrainingProgram__panel}
+              text={t('TrainingProgram.total')}
+              description={t('TrainingProgram.task.two.description')}
+            />
           </div>
-          <Panel
-            className={s.TrainingProgram__panel}
-            text={t('TrainingProgram.total')}
-            description={t('TrainingProgram.task.four.description')}
-          />
+          <div className={s.TrainingProgram__title}>
+            <p>{t('TrainingProgram.task.three.title')}</p>
+            <p>03</p>
+          </div>
+          <div className={s.TrainingProgram__tasks}>
+            <div className={s.TrainingProgram__wrapper}>
+              <List items={trainingReact} />
+            </div>
+            <Panel
+              className={s.TrainingProgram__panel}
+              text={t('TrainingProgram.total')}
+              description={t('TrainingProgram.task.three.description')}
+            />
+          </div>
+          <div className={s.TrainingProgram__title}>
+            <p>{t('TrainingProgram.task.four.title')}</p>
+            <p>04</p>
+          </div>
+          <div className={s.TrainingProgram__tasks}>
+            <div className={s.TrainingProgram__wrapper}>
+              <List items={trainingJob} />
+            </div>
+            <Panel
+              className={s.TrainingProgram__panel}
+              text={t('TrainingProgram.total')}
+              description={t('TrainingProgram.task.four.description')}
+            />
+          </div>
         </div>
       </Container>
     </Section>
