94973250c029ad0cbcb1108edcf74139a28f234c
Anatoly <anatolykostarev@gmail.com>
2022-11-29 03:55:50 +0500

    Create component, add desktop styles

---
+++ src/components/BlogArticle/BlogArticle.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import clsx from 'clsx'
+import play from './icon-play.svg'
+import s from './BlogArticle.module.sass'
+
+export const BlogArticle = ({
+  className,
+  image,
+  caption,
+  description,
+  route,
+}) => {
+  return (
+    <article className={clsx(s.blogArticle, className)}>
+      <div className={s.blogArticle__pic}>
+        <Link to={route}>
+          <img
+            src={image}
+            alt=""
+            width="500"
+            height="190"
+          />
+        </Link>
+        <div className={s.blogArticle__play}>
+          <img
+            className={s.blogArticle__img}
+            src={play}
+            alt="play"
+            width="58"
+            height="58"
+          />
+        </div>
+      </div>
+      <div className={s.blogArticle__info}>
+        <Link to={route}>
+          <p>
+            {caption}
+          </p>
+        </Link>
+        <Link to={route}>
+          <p>
+            {description}
+          </p>
+        </Link>
+      </div>
+    </article>
+  )
+}
