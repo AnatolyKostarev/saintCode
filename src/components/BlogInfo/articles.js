180a274bea7d4bd93122a15932c233204483b7df
Anatoly <anatolykostarev@gmail.com>
2022-11-29 04:03:22 +0500

    Add boolean to render play icon (#83)

--- src/components/BlogInfo/articles.js
+++ src/components/BlogInfo/articles.js
@@ -5,12 +5,12 @@ import time from './img/image-time.jpg'
 
 export const articles = [
   {
-    id: uuidv4(), image: backEnd, caption: 'Тренинг собеседования Junior Backend Developer', description: 'Тренинг собеседования с техническим директором. Запись с занятий по трудоустройству для начинающих веб-разработчиков в SaintCode Bootcamp', route: '*',
+    id: uuidv4(), image: backEnd, caption: 'Тренинг собеседования Junior Backend Developer', description: 'Тренинг собеседования с техническим директором. Запись с занятий по трудоустройству для начинающих веб-разработчиков в SaintCode Bootcamp', route: '*', icon: true,
   },
   {
-    id: uuidv4(), image: frontEnd, caption: 'Тренинг собеседования Junior Frontend Developer', description: 'Тренинг собеседования с техническим директором. Запись с занятий по трудоустройству для начинающих веб-разработчиков в SaintCode Bootcamp', route: '*',
+    id: uuidv4(), image: frontEnd, caption: 'Тренинг собеседования Junior Frontend Developer', description: 'Тренинг собеседования с техническим директором. Запись с занятий по трудоустройству для начинающих веб-разработчиков в SaintCode Bootcamp', route: '*', icon: true,
   },
   {
-    id: uuidv4(), image: time, caption: 'Беспощадное время', description: 'Статья о работе с датой и временем в программировании. Автор — Игорь Поддубный, эксперт SaintCode Bootcamp', route: '*',
+    id: uuidv4(), image: time, caption: 'Беспощадное время', description: 'Статья о работе с датой и временем в программировании. Автор — Игорь Поддубный, эксперт SaintCode Bootcamp', route: '*', icon: false,
   },
 ]
