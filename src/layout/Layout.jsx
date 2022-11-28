a923c6935398cd4821e14b0777a1455179552631
Danil <mydogfromhell666@gmail.com>
2022-11-28 04:21:37 +0300

    add toTop component

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
+import { ToTop } from '../components/ToTop'
 
 export const Layout = ({ children }) => {
+    const [topIsActive, setTopIsActive] = useState(false)
+
+    const changeHeader = () => {
+        window.scrollY >= 1000
+            ? setTopIsActive(true)
+            : setTopIsActive(false)
+    }
+    window.addEventListener('scroll', changeHeader)
+
   return (
     <>
       <Header />
       <main>
         {children}
       </main>
+        {topIsActive &&
+            <ToTop/>
+        }
       <Footer />
     </>
   )
