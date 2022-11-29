07e8d19c097b23f27854960f2632c8c74ee49517
Anatoly <anatolykostarev@gmail.com>
2022-11-29 13:18:40 +0500

    Add ScrollToTop for any page

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ScrollToTop } from './utils/scrollToTop'
 import App from './App/App'
 import './index.sass'
 import i18n from './config/i18n/i18n'
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
     <React.StrictMode>
+      <ScrollToTop />
       <App />
     </React.StrictMode>
   </BrowserRouter>,
