5a9885b202870c1a262775c6713230f421fab210
Anatoly <anatolykostarev@gmail.com>
2022-11-29 12:54:40 +0500

    Add feature to show short/full atricles list, add button, add styles (#86)
    * Fix styles
    
    * Create array to describe component
    
    * Add component images
    
    * Add button styles
    
    * Add functionality to show cuted/full articles list

--- src/components/BlogInfo/BlogInfo.jsx
+++ src/components/BlogInfo/BlogInfo.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Section } from '../../ui/Section'
 import { Container } from '../../ui/Container'
 import { BlogTitle } from '../BlogTitle/BlogTitle'
 import { BlogArticle } from '../BlogArticle'
+import { Button } from '../../ui/Button'
 import { articles } from './articles'
 import s from './BlogInfo.module.sass'
 
 export const BlogInfo = () => {
+  const [slice, setSlice] = useState(true)
+
   return (
     <Section className={s.blogInfo}>
       <Container>
         <BlogTitle />
-        <div className={s.blogInfo__articles}>
-          {articles.map(article => (
-            <BlogArticle {...article} key={article.id} />
+        <div className={s.blogInfo__articles} style={slice ? { paddingBottom: '60px' } : { paddingBottom: '7px' }}>
+          {(slice ? articles.slice(0, 3) : articles).map(article => (
+            <BlogArticle
+              {...article}
+              key={article.id}
+            />
           ))}
         </div>
+        {slice && (
+          <Button
+            className={s.blogInfo__btn}
+            text="Смотреть ещё"
+            type="button"
+            onClick={() => setSlice(false)}
+          />
+        )}
       </Container>
     </Section>
   )
