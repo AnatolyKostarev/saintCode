72b855df2fdfe1ce7df467a6c9814c08e6a757a3
Anatoly <anatolykostarev@gmail.com>
2022-11-30 17:48:11 +0500

    Add translations Hero (#93)

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { Section } from '../../ui/Section'
 import { Container } from '../../ui/Container'
 import { Title } from '../../ui/Title'
@@ -10,27 +11,37 @@ export const Hero = () => {
   const HERO_BG = {
     backgroundImage: `url(${heroBG})`,
   }
+
+  const { t } = useTranslation()
+
   return (
     <Section style={HERO_BG} className={s.Hero}>
       <Container>
         <p className={s.Hero__subtitle}>
-          Перезапусти карьеру&nbsp;
+          {t('Hero.title')}
+          &nbsp;
+          {/* Перезапусти карьеру&nbsp; */}
           <span className={s.mark}>
-            — научись программировать
+            — {t('Hero.message')}
+            {/* — научись программировать */}
           </span>
         </p>
         <HeroArticle>
           <Title>
-            Школа программирования
+            {t('Hero.school')}
+            {/* Школа программирования */}
             <br />
             <span className={s.mark}>
-              для&nbsp;веб-разработчиков в&nbsp;формате буткемпа:
+              {t('Hero.web')}
+              {/* для&nbsp;веб-разработчиков в&nbsp;формате буткемпа: */}
             </span>
             <br />
-            с&nbsp;0&nbsp;до&nbsp;Junior за&nbsp;3&nbsp;месяца,
+            {t('Hero.junior')}
+            {/* с&nbsp;0&nbsp;до&nbsp;Junior за&nbsp;3&nbsp;месяца, */}
             <br />
             <span className={s.mark}>
-              вживую в&nbsp;СПб&nbsp;или&nbsp;онлайн из&nbsp;любой точки
+              {t('Hero.offLine')}
+              {/* вживую в&nbsp;СПб&nbsp;или&nbsp;онлайн из&nbsp;любой точки */}
             </span>
           </Title>
         </HeroArticle>
