06b5d4fff1a62ba36b2e93995bbcda6f60010eec
Anatoly <anatolykostarev@gmail.com>
2022-12-01 19:02:57 +0500

    Create Time article

--- src/page/Blog/Time/Time.jsx
+++ src/page/Blog/Time/Time.jsx
@@ -4,6 +4,10 @@ import { Layout } from '../../../layout'
 import { Section } from '../../../ui/Section'
 import { Container } from '../../../ui/Container'
 import { SectionTitle } from '../../../ui/SectionTitle'
+import { BcgAttachment } from '../../../ui/BcgAttachment'
+import time from './bcg-time.jpg'
+import decoration from './icon-decoration.svg'
+import avatar from './icon-Poddubny.png'
 import s from './Time.module.sass'
 
 export const Time = ({ className }) => {
@@ -17,12 +21,40 @@ export const Time = ({ className }) => {
           <p className={s.Time__subtitle}>
             или пара слов о работе с датой и временем в программировании
           </p>
-          <div className={s.Time__bcg} />
+          <BcgAttachment className={s.Time__bcg} backgroundImage={time} />
         </Container>
       </Section>
       <Section className={s.Time__description}>
         <Container>
-          2 ctrwbz
+          <p className={s.Time__preface}>
+            Возможно, прочитав название, вы подумали, что опять какой-то
+            пенсионер-разработчик (Senior Developer) будет философствовать на
+            тему того, как быстро летит время и что надо постоянно развиваться,
+            чтобы оставаться на плаву, а чтобы двигаться вперёд, надо
+            развиваться ещё больше. Всё это, безусловно, правда, но всё-таки
+            хочется рассказать о практических проблемах в программировании, с
+            которыми сталкиваются начинающие разработчики.
+          </p>
+          <div className={s.Time__decoration}>
+            <img
+              src={decoration}
+              alt="line"
+            />
+          </div>
+          <div className={s.Time__author}>
+            <div className={s.Time__avatar}>
+              <img
+                src={avatar}
+                alt="Poddubny"
+                width="80"
+                height="80"
+              />
+            </div>
+            <div className={s.Time__info}>
+              <p>Игорь Поддубный</p>
+              <p>Автор статьи. Разработчик</p>
+            </div>
+          </div>
         </Container>
       </Section>
     </Layout>
