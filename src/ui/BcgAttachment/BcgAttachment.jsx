4b9222a2040d860579bd4253ca786488a413e296
Anatoly <anatolykostarev@gmail.com>
2022-12-03 20:55:44 +0500

    Refactor background attachment component

--- src/ui/BcgAttachment/BcgAttachment.jsx
+++ src/ui/BcgAttachment/BcgAttachment.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import clsx from 'clsx'
 import s from './BcgAttachment.module.sass'
 
-export const BcgAttachment = ({ className }) => {
-  return <div className={clsx(s.BcgAttachment, className)} />
+export const BcgAttachment = ({ className, children }) => {
+  return (
+    <div className={clsx(s.BcgAttachment, className)}>
+      {children}
+    </div>
+  )
 }
