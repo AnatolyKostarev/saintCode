92fa68e2c29439d80433079ecdc70edee011b91c
Anatoly <anatolykostarev@gmail.com>
2022-12-07 22:43:52 +0500

    Add lang switcher

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -83,6 +83,7 @@ export const Header = () => {
             />
           </div>
         </div>
+        <LangSwitcher />
         {isConsultForm && <ConsultForm setIsConsultForm={setIsConsultForm} />}
       </Container>
     </header>
