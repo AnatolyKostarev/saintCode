bc2799306f36c8470efc5776a9e8252ba435936b
Anatoly <anatolykostarev@gmail.com>
2022-12-08 17:29:08 +0500

    Add responsive styles for Fronend and Backend interview pages, refactor code (#102)
    * Refactor component, fix styles
    
    * Refactor code, fix responsive styles
    
    * Fix styles, and responsive styles

--- src/ui/BcgAttachment/BcgAttachment.jsx
+++ src/ui/BcgAttachment/BcgAttachment.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 import clsx from 'clsx'
 import clock from './bcg-clock.jpg'
 import s from './BcgAttachment.module.sass'
+import { Container } from '../Container'
 
 export const BcgAttachment = ({ className, children }) => {
   return (
-    <div className={clsx(s.BcgAttachment, className)}>
-      {children}
-      <img
-        className={s.BcgAttachment__bcg}
-        src={clock}
-        alt=""
-        width="1160"
-        height="482"
-      />
-    </div>
+    <Container className={s.BcgAttachment__container}>
+      <div className={clsx(s.BcgAttachment, className)}>
+        {children}
+        <img
+          className={s.BcgAttachment__bcg}
+          src={clock}
+          alt=""
+          width="1160"
+          height="482"
+        />
+      </div>
+    </Container>
   )
 }
