9ac875ba7d00fa0084ab9c7ba8b776ac84a5a2a7
Anatoly <anatolykostarev@gmail.com>
2022-12-16 11:50:45 +0500

    Add translations for Slider

--- src/components/StudentStory/StudentStory.jsx
+++ src/components/StudentStory/StudentStory.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import clsx from 'clsx'
 import { Section } from '../../ui/Section'
 import { Container } from '../../ui/Container'
@@ -7,11 +8,12 @@ import { Slider } from '../Slider'
 import s from './StudentStory.module.sass'
 
 export const StudentStory = () => {
+  const { t } = useTranslation()
   return (
     <Section className={clsx(s.StudentStory)}>
       <Container className={s.container}>
         <SectionTitle className={s.title}>
-          Истории студентов
+          {t('StudentStory')}
         </SectionTitle>
       </Container>
       <Slider />
