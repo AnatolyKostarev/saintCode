0d2f89c597ccddd5f7f539e4efc2ffe2db6c2d19
Danil <mydogfromhell666@gmail.com>
2023-01-08 16:30:32 +0300

    fix attachment, add policy modal

--- src/ui/FooterCopyright/FooterCopyright.jsx
+++ src/ui/FooterCopyright/FooterCopyright.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import clsx from 'clsx'
 import { Container } from '../Container'
 import { Button } from '../Button'
 import s from './FooterCopyright.module.sass'
+import { Modal } from '../../components/Modal'
+import { Policy } from '../../components/Policy'
 
 export const FooterCopyright = ({ className }) => {
   const { t } = useTranslation()
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <div className={clsx(s.FooterCopyright, className)}>
@@ -20,7 +23,10 @@ export const FooterCopyright = ({ className }) => {
             {t('FooterCopyright.text')}
           </p>
         </div>
-        <Button className={clsx(s.policy)} text={t('FooterCopyright.policy')} />
+        <Button onClick={() => setIsOpen(true)} className={clsx(s.policy)} text={t('FooterCopyright.policy')} />
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+          <Policy />
+        </Modal>
       </Container>
     </div>
   )
